4117c7ea9893dfbafd12844ebe40066553d8ae5e
Alberonda <albertoreyesmartin@gmail.com>
2017-05-11 11:07:31 +0200

    First commit

---
+++ test/prfactorial/FactorialTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prfactorial;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alumno
+ */
+public class FactorialTest {
+    
+    public FactorialTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void factorailEntrada0Salida1() {
+        assertEquals(new Integer(1), Factorial.calcular(0));
+    }
+    
+    
+    
+}
