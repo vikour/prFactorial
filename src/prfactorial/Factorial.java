2209d14057da9faca135a3eedf518efde67dedac
Alberonda <albertoreyesmartin@gmail.com>
2017-05-11 11:17:28 +0200

    Primer test pasado

--- src/prfactorial/Factorial.java
+++ src/prfactorial/Factorial.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prfactorial;
-
-/**
- *
- * @author alumno
- */
 public class Factorial {
 
     static Object calcular(int i) {
-        return 0;
+        return 1;
     }
 }
